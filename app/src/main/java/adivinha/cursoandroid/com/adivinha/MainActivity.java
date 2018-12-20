ff3c970e404fac84800e5d1ab0d4bd930c6394d8
Junior Martins <valter.martins@metasix.com.br>
2018-12-20 19:54:34 -0200

    Secao7,aula49 - fim

--- app/src/main/java/adivinha/cursoandroid/com/adivinha/MainActivity.java
+++ app/src/main/java/adivinha/cursoandroid/com/adivinha/MainActivity.java
@@ -2,12 +2,22 @@ package adivinha.cursoandroid.com.adivinha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button botaoJogar;
+    private TextView textoResultado;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        botaoJogar = (Button) findViewById(R.id.botaoJogarId);
+        textoResultado = (TextView) findViewById(R.id.resultadoId);
+
+        textoResultado.setText("Texto alterado");
     }
 }
