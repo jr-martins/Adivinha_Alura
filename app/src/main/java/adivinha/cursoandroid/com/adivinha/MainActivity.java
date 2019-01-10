3101c4cda19a04e8b6bae7ef60c01d1f85fb4866
Junior Martins <valter.martins@metasix.com.br>
2019-01-10 15:14:14 -0200

    Fim do App Advinha

--- app/src/main/java/adivinha/cursoandroid/com/adivinha/MainActivity.java
+++ app/src/main/java/adivinha/cursoandroid/com/adivinha/MainActivity.java
@@ -2,8 +2,11 @@ package adivinha.cursoandroid.com.adivinha;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
+
+import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -18,6 +21,30 @@ public class MainActivity extends AppCompatActivity {
         botaoJogar = (Button) findViewById(R.id.botaoJogarId);
         textoResultado = (TextView) findViewById(R.id.resultadoId);
 
-        textoResultado.setText("Texto alterado");
+
+
+        botaoJogar.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                Random randomico = new Random();
+
+                int numeroAleatorio = randomico.nextInt(10);
+
+                if(numeroAleatorio == 5)
+                {
+                    textoResultado.setText("Você ganhou: " + numeroAleatorio);
+                }else{
+
+                    textoResultado.setText("Número escolhido: " + numeroAleatorio);
+
+                }
+
+
+
+
+
+            }
+        });
     }
 }
